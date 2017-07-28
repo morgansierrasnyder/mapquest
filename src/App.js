0c2ab25297cc5f34fc1ffc4d607860effdd11dfd
Morgan Snyder <msnyder@classy.org>
2017-07-27 20:50:41 -0700

    dependencies, static google maps loading in index.html

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import glamorous from 'glamorous'
+
+import logo from './assets/logo.svg'
+import { Map } from './components'
+
+const Container = glamorous.div({
+  textAlign: 'center',
+  margin: 0,
+  padding: 0,
+  fontFamily: 'sans-serif'
+})
+
+const Header = glamorous.div({
+  backgroundColor: '#aaa',
+  height: '150px',
+  padding: '20px',
+  color: '#fff'
+})
+
+const Logo = glamorous.img({
+  height: '60px'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <Container>
+        <Header>
+          <Logo src={logo} alt="edcorps logo" />
           <h2>Real World Scholars<br/>EdCorps Map</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        </Header>
+        <p>
+          We're on a quest to make a map of all Real World Scholars EdCorps classrooms!
         </p>
-      </div>
-    );
+        <Map></Map>
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
